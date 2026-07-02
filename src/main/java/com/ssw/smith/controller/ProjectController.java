b226d975212b1b55924b2565a1eade84891a29e3
saltsodawater <827743509@qq.com>
2026-07-02 21:37:58 +0800

    细节调整

--- src/main/java/com/ssw/smith/controller/ProjectController.java
+++ src/main/java/com/ssw/smith/controller/ProjectController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/projects")
+@RequestMapping("/projects")
 public class ProjectController {
 
     private final ProjectService projectService;
--- src/main/java/com/ssw/smith/controller/TraceController.java
+++ src/main/java/com/ssw/smith/controller/TraceController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/traces")
+@RequestMapping("/traces")
 public class TraceController {
 
     private final TraceService traceService;
